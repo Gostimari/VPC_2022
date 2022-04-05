c77a2f9135af195913cc6bbc860c4c9580d99b6d
Duarte Cruz <duda__9@hotmail.com>
2022-04-05 15:36:20 +0100

    Matching features implementation

--- Assignment#2/Assignment#2/matlab/FeatureDescriptor.m
+++ Assignment#2/Assignment#2/matlab/FeatureDescriptor.m
@@ -17,19 +17,19 @@ function [Descriptors] = FeatureDescriptor(Img,Pts,Dscpt_type,Patch_size)
         for i = 1:size(Pts.x,1)
             N = Patch_size;
             window = zeros(N); % Simple window 5x5 square
-            Descriptors = zeros(size(Pts.x,1),N*N); %Array with the number of rows of keypoints and columns with dimension of the feature
+            Descriptors = zeros(size(Pts.x,1),8*8); %Array with the number of rows of keypoints and columns with dimension of the feature
             
             imgpad = padarray(Img,[floor(N/2) floor(N/2)],'replicate','both'); % Increases image in half the size of N 
-            window = imgpad(Pts.x(i)+floor(N/2)-2:Pts.x(i)+floor(N/2)+2,Pts.y(i)+floor(N/2)-2:Pts.y(i)+floor(N/2)+2); % 5x5 window centered in the keypoint
-            B = normalize(window);
+            window = imgpad(Pts.x(i)+floor(N/2)-20:Pts.x(i)+floor(N/2)+20,Pts.y(i)+floor(N/2)-20:Pts.y(i)+floor(N/2)+20); % 5x5 window centered in the keypoint
 
-            angle = -Pts.o(i);
-            TR= [cosd(angle) sind(angle) 0;-sind(angle) cosd(angle) 0;0 0 1];
+            angle = Pts.o(i);
+            TR= [cosd(angle) -sind(angle) 0; sind(angle) cosd(angle) 0;0 0 1];
             Tform = affine2d(TR);
 
-            A = imwarp(B, Tform);
-            resized = imresize(A,[N N]);
-            Descriptors(i,:) = reshape(resized,1,N*N);
+            A = imwarp(window, Tform);
+            resized = imresize(A,[8 8]);
+            Descriptors(i,:) = reshape(resized,1,8*8);
+            Descriptors(i,:) = normalize(Descriptors(i,:));
         end
 
     end
--- Assignment#2/Assignment#2/matlab/ShowMatching.m
+++ Assignment#2/Assignment#2/matlab/ShowMatching.m
@@ -4,17 +4,15 @@ function ShowMatching(MatchList,img1,img2,Dscpt1,Dscpt2)
 % Allow also the possibility to visualise the 8x8 (or 5x5) feature patches
 % per matching.
 
+figure()
+imshow(img1,[]);
+hold on
 for i = 1:size(MatchList,1) %For every element that is a supposed match
-    %Gets the correct match
-    p=H*[Pts1.y(MatchList(i,1));Pts1.x(MatchList(i,1));1]; 
-    xa=floor(p(1)/p(3)); 
-    ya=floor(p(2)/p(3));
-    
-    %Gets the supposed match
-    xt=Pts2.y(MatchList(i,2));
-    yt=Pts2.x(MatchList(i,2));
+    for j = 1:size(MatchList, 2)
+        plot(MatchList(i,j), '--ro');
+    end
 
 end
-
+hold off
 end
         
